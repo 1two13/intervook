b6cee17427a1fb2c41f0e0127f516c904bc8bd8e
Tory <88531407+1two13@users.noreply.github.com>
2022-10-17 22:30:51 +0900

    사람 아이콘 hover 했을 때 드롭다운 메뉴 보여주기

--- src/assets/Icons.tsx
+++ src/assets/Icons.tsx
@@ -3,11 +3,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 interface IIcons {
   icon: any;
   onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
+  onMouseOver?: (e: React.MouseEvent<HTMLDivElement>) => void;
+  onMouseOut?: (e: React.MouseEvent<HTMLDivElement>) => void;
 }
 
-function Icons({ icon, onClick }: IIcons) {
+function Icons({ icon, onClick, onMouseOver, onMouseOut }: IIcons) {
   return (
-    <div onClick={onClick}>
+    <div onClick={onClick} onMouseOver={onMouseOver} onMouseOut={onMouseOut}>
       <FontAwesomeIcon
         size="2x"
         color="lightGreen"
--- src/common/components/Logged.tsx
+++ src/common/components/Logged.tsx
@@ -1,23 +1,39 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { faBookmark } from "@fortawesome/free-regular-svg-icons";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
+
 import Icons from "../../assets/Icons";
+import IsHover from "./IsHover";
 
 function Logged() {
   const navigate = useNavigate();
+  const [isHover, setIsHover] = useState(false);
 
   const moveToMyPage = () => {
     navigate("/mypage");
   };
 
   return (
-    <>
+    <div className="flex relative">
       <Icons icon={faHeart} />
       <Icons icon={faBookmark} />
-      <Icons icon={faUser} onClick={moveToMyPage} />
-    </>
+
+      <div
+        onMouseOver={() => {
+          setIsHover(true);
+        }}
+        onMouseOut={() => {
+          setIsHover(false);
+        }}
+      >
+        <Icons icon={faUser} onClick={moveToMyPage} />
+        {/* isHover가 true일 때만 IsHover 컴포넌트 렌더링 */}
+        {isHover && <IsHover />}
+      </div>
+    </div>
   );
 }
 
