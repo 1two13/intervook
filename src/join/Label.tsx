e3cc7c78a0b9fbff9a5e23cb01b771a9fb33dc39
Tory <88531407+1two13@users.noreply.github.com>
2023-01-03 17:35:40 +0900

    refactor(Label): interface명 변경

--- src/join/Label.tsx
+++ src/join/Label.tsx
@@ -1,8 +1,8 @@
-interface ITitle {
+interface ILabel {
   name: string;
 }
 
-function Label({ name }: ITitle) {
+function Label({ name }: ILabel) {
   return <div className="flex w-[100%]">{name}</div>;
 }
 
