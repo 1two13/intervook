ef5680aff6c97a21c8583e19e38e4d32222955e6
Tory <88531407+1two13@users.noreply.github.com>
2023-01-03 23:46:17 +0900

    refactor(index): store 객체로 감싸주기

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux-toolkit/store';
+import { store } from './redux-toolkit/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
