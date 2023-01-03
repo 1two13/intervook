f7fe0f9602880214e4de12c50465d9e7dbba3b0a
Tory <88531407+1two13@users.noreply.github.com>
2023-01-03 17:36:09 +0900

    style: 주석 추가

--- src/join/Email.tsx
+++ src/join/Email.tsx
@@ -12,7 +12,9 @@ function Email() {
   const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
     const emailRegex =
       /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+
     setEmail(e.target.value);
+
     if (!emailRegex.test(e.target.value)) {
       setIsEmail(false);
       setMessage('이메일 형식이 틀렸어요! 다시 확인해주세요.🥲');
