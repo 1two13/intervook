960946033e323516eb7c8fed99ec3d623d8702b7
Tory <88531407+1two13@users.noreply.github.com>
2023-01-03 17:28:41 +0900

    refactor(Email): state명 변경,  placeholder 변경

--- src/join/components/Email.tsx
+++ src/join/components/Email.tsx
@@ -6,7 +6,7 @@ import Comments from './Comments';
 
 function Email() {
   const [email, setEmail] = useState<string>('');
-  const [emailMsg, setEmailMsg] = useState<string>('');
+  const [message, setMessage] = useState<string>('');
   const [isEmail, setIsEmail] = useState<boolean>(false);
 
   const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -14,19 +14,24 @@ function Email() {
       /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
     setEmail(e.target.value);
     if (!emailRegex.test(e.target.value)) {
-      setEmailMsg('이메일 형식이 틀렸어요! 다시 확인해주세요.🥲');
       setIsEmail(false);
+      setMessage('이메일 형식이 틀렸어요! 다시 확인해주세요.🥲');
     } else {
-      setEmailMsg('올바른 이메일 형식입니다.');
       setIsEmail(true);
+      setMessage('올바른 이메일 형식입니다.');
     }
   };
 
   return (
     <>
       <Label name={'이메일'} />
-      <Input placeholder="example@gotori.com" name="email" type="text" onChange={onChangeEmail} />
-      {email.length > 0 ? <Comments message={emailMsg} /> : ''}
+      <Input
+        placeholder="example@interviewbook.com"
+        name="email"
+        type="text"
+        onChange={onChangeEmail}
+      />
+      {email.length > 0 ? <Comments message={message} /> : ''}
     </>
   );
 }
