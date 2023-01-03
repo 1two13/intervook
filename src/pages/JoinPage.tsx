727a02efc5e036b2970dc6cbabac1d4eacdc27eb
Tory <88531407+1two13@users.noreply.github.com>
2023-01-03 23:14:45 +0900

    refactor(JoinPage): 사용하지 않는 state들 제거

--- src/pages/JoinPage.tsx
+++ src/pages/JoinPage.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 import LoginNJoinBox from '../common/components/LoginNJoinBox';
 import H1 from '../common/components/H1';
 import Name from '../join/Name';
@@ -8,20 +6,16 @@ import Password from '../join/Password';
 import Button from '../join/Button';
 
 function JoinPage() {
-  const [isName, setIsName] = useState<boolean>(false);
-  const [isEmail, setIsEmail] = useState<boolean>(false);
-  const [isPassword, setIsPassword] = useState<boolean>(false);
-
   return (
     <div className="flex h-screen justify-center items-center">
       <LoginNJoinBox>
         <H1 name={'회원가입'} />
 
         <form className="flex flex-col">
-          <Name setIsName={setIsName} />
-          <Email setIsEmail={setIsEmail} />
-          <Password setIsPassword={setIsPassword} />
-          <Button isCorrect={[isName, isEmail, isPassword]} />
+          <Name />
+          <Email />
+          <Password />
+          <Button />
         </form>
       </LoginNJoinBox>
     </div>
