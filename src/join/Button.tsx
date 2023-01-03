8850816450a60fa0b56ce82340c73d910cd8d42c
Tory <88531407+1two13@users.noreply.github.com>
2023-01-03 23:12:37 +0900

    refactor(Button): useSelector로 state 꺼내기

--- src/join/Button.tsx
+++ src/join/Button.tsx
@@ -1,22 +1,25 @@
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-function Button({ isCorrect }: { isCorrect: Array<boolean> }) {
-  const truthy = isCorrect.every((el) => el === true);
-
+function Button() {
+  const selector = useSelector((state: any) => {
+    return [state.join.isName, state.join.isEmail, state.join.isPassword];
+  });
+  const isValidate = selector.every((el) => el === true);
   const navigate = useNavigate();
   const onSubmitHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    if (truthy) navigate('/');
+    if (isValidate) navigate('/');
   };
 
   return (
     <button
       className={
-        truthy
+        isValidate
           ? 'w-[260px] h-[50px] pl-[10px] mt-[16px] text-[16px] border-[0.7px] rounded-[7px] bg-lightGreen text-[white]'
           : 'w-[260px] h-[50px] pl-[10px] mt-[16px] text-[16px] border-[0.7px] rounded-[7px] bg-deepGray text-[white]'
       }
-      disabled={!truthy}
+      disabled={!isValidate}
       onClick={onSubmitHandler}
     >
       회원가입
