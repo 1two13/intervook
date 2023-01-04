266498f46b2ee9f2b246ff3cc10b90637412c239
Tory <88531407+1two13@users.noreply.github.com>
2023-01-04 16:14:05 +0900

    feat(ShowLink): 이벤트 핸들러 추가, style 변경

--- src/writing/ShowLink.tsx
+++ src/writing/ShowLink.tsx
@@ -1,9 +1,23 @@
+import { useState } from 'react';
+
 function ShowLink() {
+  const [link, setLink] = useState<string>('');
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLink(e.target.value);
+  };
+
   return (
-    <input
-      placeholder="https://"
-      className="w-[100%] h-[60px] mt-[20px] pl-[10px] outline-0 rounded-[5px] border-[1px] border-lightGray"
-    />
+    <div className="flex w-[100%] h-[60px] mt-[20px] border-[1px] border-lightGray">
+      <input
+        onChange={onChange}
+        placeholder="https://"
+        className="w-[95%] pl-[10px] outline-0 rounded-[5px]"
+      />
+      {/* <button onClick={onClick} className="w-[5%] pr-[10px] text-deepGray text-[25px]">
+        +
+      </button> */}
+    </div>
   );
 }
 
