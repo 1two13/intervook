d614055b012c640f787271d79b9aac5913cfc141
Tory <88531407+1two13@users.noreply.github.com>
2023-01-04 13:06:20 +0900

    style: 주석 제거

--- src/main/components/Buttons.tsx
+++ src/main/components/Buttons.tsx
@@ -1,12 +1,12 @@
-import Button from "../../common/components/Button";
+import Button from '../../common/components/Button';
 
 function Buttons() {
   return (
     <div className="flex">
-      <Button name={"최신 순"} />
-      <Button name={"이름 순"} />
-      <Button name={"좋아요 순"} />
-      <Button name={"북마크 순"} />
+      <Button name={'최신 순'} />
+      <Button name={'이름 순'} />
+      <Button name={'좋아요 순'} />
+      <Button name={'북마크 순'} />
     </div>
   );
 }
