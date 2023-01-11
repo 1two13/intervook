82bfc47a701d15c29db667992c99e1ab15f39107
Tory <88531407+1two13@users.noreply.github.com>
2023-01-11 23:59:24 +0900

    style: 작은 따옴표로 변경, placeholder 변경

--- src/components/myPage/ProfileNIntro.tsx
+++ src/components/myPage/ProfileNIntro.tsx
@@ -1,8 +1,8 @@
-import { useRef, useState } from "react";
+import { useRef, useState } from 'react';
 
 function Profile() {
   const [imageUrl, setImageUrl] = useState(
-    "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+    'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
   );
   const imgRef = useRef<HTMLInputElement>(null);
 
@@ -44,14 +44,14 @@ function Profile() {
       </div>
 
       <input
-        placeholder="고토리"
+        placeholder="고주디"
         className="w-[23%] py-[3px] mt-[15px] mb-[60px] text-center text-[black] bg-lightGray/50 focus:outline-none"
       />
 
       <div className="w-[50%]">
         <div className="text-[18px]">자기소개</div>
         <input
-          placeholder="프론트엔드 개발자가 되고 싶은 고토리입니다!"
+          placeholder="프론트엔드 개발자가 되고 싶은 고주디입니다!"
           className="flex w-[100%] h-[140px] mb-[120px] p-[15px] justify-between text-[black] shadow-md bg-lightGray/10 focus:outline-none"
         />
       </div>
