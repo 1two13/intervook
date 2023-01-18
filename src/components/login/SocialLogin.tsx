b4aeac81b3cb9762e2741b1e3b2cbc1fe99ef70e
Tory <88531407+1two13@users.noreply.github.com>
2023-01-18 17:06:20 +0900

    refactor: 간편로그인(카카오, 네이버) 이미지 추가

--- src/components/login/SocialLogin.tsx
+++ src/components/login/SocialLogin.tsx
@@ -1,20 +1,22 @@
 import Hr from './Hr';
-import Button from './Button';
+import kakaoLogin from '../../assets/images/kakaoLogin.png';
+import naverLogin from '../../assets/images/naverLogin.png';
 
 function SocialLogin() {
   return (
     <>
-      <div className="w-[100%] flex items-center justify-center">
+      <div className="w-[100%] flex mb-[2%] items-center justify-center">
         <Hr />
         <div className="text-[14px] px-[5px] text-deepGray text-center">간편 로그인</div>
         <Hr />
       </div>
 
-      <div className="flex w-[100%] justify-between">
-        <Button name={'깃'} />
-        <Button name={'구'} />
-        <Button name={'카'} />
-      </div>
+      <a href={''}>
+        <img src={kakaoLogin} alt="카카오 로그인 버튼" className="w-[70%] m-auto" />
+      </a>
+      <a href={''} className="mt-[2%]">
+        <img src={naverLogin} alt="네이버 로그인 버튼" className="w-[40%] m-auto" />
+      </a>
     </>
   );
 }
