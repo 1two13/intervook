e054e5dd42b8f9f0a8d35813f67e4db86554da65
Tory <88531407+1two13@users.noreply.github.com>
2023-01-18 16:51:45 +0900

    style: 회원가입 박스 CSS 수정

--- src/components/common/components/LoginNJoinBox.tsx
+++ src/components/common/components/LoginNJoinBox.tsx
@@ -1,6 +1,6 @@
 function LoginNJoinBox({ children }: any) {
   return (
-    <div className="flex flex-col px-[5%] py-[5%] justify-center items-center border-[0.7px] border-deepGray rounded-[15px] bg-[white]">
+    <div className="flex flex-col max-w-[25%] px-[3%] py-[2%] justify-center items-center border-[0.7px] border-deepGray rounded-[15px] bg-[white]">
       {children}
     </div>
   );
