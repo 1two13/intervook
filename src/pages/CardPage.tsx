bc59075d65fcd51c83b744ff29380fa82aeb004f
Tory <88531407+1two13@users.noreply.github.com>
2023-01-18 14:18:28 +0900

    refactor: 링크 박스 수정

--- src/pages/CardPage.tsx
+++ src/pages/CardPage.tsx
@@ -1,4 +1,8 @@
 function CardPage() {
+  const link = () => {
+    window.open('https://www.naver.com/', '_blank');
+  };
+
   return (
     <div className="w-[70%] m-auto px-[3%] pt-[2%] bg-[white] border-[1px] border-lightGray">
       <div className="mb-[2%]">
@@ -10,9 +14,8 @@ function CardPage() {
       </div>
 
       <div className="h-[100vh] mb-[1%]">작성된 글...</div>
-      <div>
-        링크
-        <link href="https://www.naver.com/" />
+      <div onClick={link} className="w-[100%] mb-[5%] rounded-[5%] cursor-pointer bg-bgColor">
+        <div className="h-[100px] p-[3%]">제목</div>
       </div>
     </div>
   );
