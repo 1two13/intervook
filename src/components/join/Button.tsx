423efdaedf657f238a19fd53c54ee4990f9eded3
Tory <88531407+1two13@users.noreply.github.com>
2023-01-21 20:01:11 +0900

    refactor: form이 onSubmit 핸들러를 가지고 있을 수 있도록 변경

--- src/components/join/Button.tsx
+++ src/components/join/Button.tsx
@@ -1,17 +1,4 @@
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-
-function Button() {
-  const selector = useSelector((state: any) => {
-    return [state.join.isName, state.join.isEmail, state.join.isPassword];
-  });
-  const isValidate = selector.every((el) => el === true);
-  const navigate = useNavigate();
-  const onSubmitHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    if (isValidate) navigate('/');
-  };
-
+function Button({ isValidate }: { isValidate: boolean }) {
   return (
     <button
       className={
@@ -20,7 +7,6 @@ function Button() {
           : 'w-[260px] h-[50px] pl-[10px] mt-[16px] text-[16px] border-[0.7px] rounded-[7px] bg-deepGray text-[white]'
       }
       disabled={!isValidate}
-      onClick={onSubmitHandler}
     >
       회원가입
     </button>
--- src/pages/JoinPage.tsx
+++ src/pages/JoinPage.tsx
@@ -4,18 +4,30 @@ import Name from '../components/join/Name';
 import Email from '../components/join/Email';
 import Password from '../components/join/Password';
 import Button from '../components/join/Button';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 function JoinPage() {
+  const navigate = useNavigate();
+  const selector = useSelector((state: any) => {
+    return [state.join.isName, state.join.isEmail, state.join.isPassword];
+  });
+  const isValidate = selector.every((el) => el === true);
+  const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (isValidate) navigate('/');
+  };
+
   return (
     <div className="flex h-screen justify-center items-center">
       <LoginJoinBox>
         <H1 name={'회원가입'} />
 
-        <form className="flex flex-col">
+        <form onSubmit={onSubmitHandler} className="flex flex-col">
           <Name />
           <Email />
           <Password />
-          <Button />
+          <Button isValidate={isValidate} />
         </form>
       </LoginJoinBox>
     </div>
