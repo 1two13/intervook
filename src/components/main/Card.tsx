eb89f2f7a97289d18daec36d87796554a67477f7
Tory <88531407+1two13@users.noreply.github.com>
2023-01-21 18:41:47 +0900

    feat: 좋아요, 북마크 버튼 클릭했을 때 색상 변경하기

--- src/components/main/Card.tsx
+++ src/components/main/Card.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -9,35 +10,46 @@ function Card() {
   const moveToCardPage = () => {
     navigate('/card');
   };
+  const [isLike, setIsLike] = useState(false);
+  const [isBookmark, setIsBookmark] = useState(false);
+  const clickLike = () => setIsLike(!isLike);
+  const clickBookmark = () => setIsBookmark(!isBookmark);
 
   return (
-    <div
-      onClick={moveToCardPage}
-      className="flex mt-[18px] mb-[30px] pt-[5px] justify-between text-deepGray shadow-md bg-[white] cursor-pointer"
-    >
-      <div className="ml-[15px] text-[12px]">
-        <div className="mb-[-9px]">작성자</div>
-        <div className="text-[24px] text-[black]">제목</div>
-        <div className="mb-[-5px] mt-[-9px]">글 미리보기</div>
-        <div>링크</div>
+    <div className="flex mt-[18px] mb-[30px] pt-[5px] justify-between text-deepGray shadow-md bg-[white] cursor-pointer">
+      <div onClick={moveToCardPage} className="w-[90%]">
+        <div className="ml-[15px] text-[12px]">
+          <div className="mb-[-9px]">작성자</div>
+          <div className="text-[24px] text-[black]">제목</div>
+          <div className="mb-[-5px] mt-[-9px]">글 미리보기</div>
+          <div>링크</div>
 
-        <div className="flex my-[14px] text-[14px]">
-          <div>#태그</div>
-          <div>#태그</div>
-          <div>#태그</div>
+          <div className="flex my-[14px] text-[14px]">
+            <div>#태그</div>
+            <div>#태그</div>
+            <div>#태그</div>
+          </div>
         </div>
       </div>
 
       <div className="flex mr-[10px]">
         <div className="mr-[10px] text-center">
-          <button>
-            <FontAwesomeIcon icon={faThumbsUp} size="2x" className="text-deepGreen" />
+          <button onClick={clickLike}>
+            <FontAwesomeIcon
+              icon={faThumbsUp}
+              size="2x"
+              className={isLike ? 'text-deepGreen' : ''}
+            />
           </button>
           <div className="text-[8px]">좋아요 7</div>
         </div>
 
-        <button className="flex pt-[2px]">
-          <FontAwesomeIcon icon={faBookmark} size="2x" className="text-deepGreen" />
+        <button onClick={clickBookmark} className="flex pt-[2px]">
+          <FontAwesomeIcon
+            icon={faBookmark}
+            size="2x"
+            className={isBookmark ? 'text-deepGreen' : ''}
+          />
         </button>
       </div>
     </div>
