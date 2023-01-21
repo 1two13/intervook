dba7ce4353d8a256d65d1a924445118d8a594798
Tory <88531407+1two13@users.noreply.github.com>
2023-01-21 19:42:27 +0900

    refactor: type 수정

--- src/components/common/components/LoginJoinBox.tsx
+++ src/components/common/components/LoginJoinBox.tsx
@@ -1,4 +1,8 @@
-function LoginJoinBox({ children }: any) {
+interface ILoginJoinBox {
+  children: React.ReactNode;
+}
+
+function LoginJoinBox({ children }: ILoginJoinBox) {
   return (
     <div className="flex flex-col max-w-[25%] px-[3%] py-[2%] justify-center items-center border-[0.7px] border-deepGray rounded-[15px] bg-[white]">
       {children}
