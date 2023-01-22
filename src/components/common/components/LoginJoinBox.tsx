573a00c33fa28dbbe46696a9df91b0b6168754a8
Tory <88531407+1two13@users.noreply.github.com>
2023-01-22 10:32:17 +0900

    refactor: width, padding 수정

--- src/components/common/components/LoginJoinBox.tsx
+++ src/components/common/components/LoginJoinBox.tsx
@@ -4,7 +4,7 @@ interface ILoginJoinBox {
 
 function LoginJoinBox({ children }: ILoginJoinBox) {
   return (
-    <div className="flex flex-col max-w-[25%] px-[3%] py-[2%] justify-center items-center border-[0.7px] border-deepGray rounded-[15px] bg-[white]">
+    <div className="flex flex-col max-w-[27%] px-[2.5%] py-[3%] justify-center items-center border-[0.7px] border-deepGray rounded-[15px] bg-[white]">
       {children}
     </div>
   );
