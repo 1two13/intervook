98b59d474b63670a3c80f476fe0d05b3f7fa3191
Tory <88531407+1two13@users.noreply.github.com>
2023-01-24 14:45:15 +0900

    fix: 프록서 서버 작동하도록 코드 추가
    55e2cdeda052411b8e76537b1ae74cb4a7f5a494

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,6 +5,12 @@ module.exports = function (app) {
     createProxyMiddleware(['/api'], {
       target: 'http://ec2-13-125-255-195.ap-northeast-2.compute.amazonaws.com:8068',
       changeOrigin: true,
+      onProxyReq: function (proxyReq, req, res, options) {
+        proxyReq.setHeader(
+          'origin',
+          'http://ec2-13-125-255-195.ap-northeast-2.compute.amazonaws.com:8068'
+        );
+      },
     })
   );
 };
