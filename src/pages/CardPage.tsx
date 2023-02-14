d04f130462081454c599a379d602ddaa1e75f0bf
Tory <88531407+1two13@users.noreply.github.com>
2023-02-14 20:58:57 +0900

    style: css 수정

--- src/pages/CardPage.tsx
+++ src/pages/CardPage.tsx
@@ -4,10 +4,10 @@ function CardPage() {
   };
 
   return (
-    <div className="w-[70%] m-auto px-[3%] pt-[2%] bg-[white] border-[1px] border-lightGray">
+    <div className="w-[70%] m-auto px-[3%] pt-[2%] bg-lightGray/5 border-[1px] border-lightGray">
       <div className="mb-[2%]">
         <div>작성자</div>
-        <div className="w-[100%] h-[55px] justify-center outline-0 text-[28px] bg-[white] border-b-[1px] border-lightGray">
+        <div className="w-[100%] h-[55px] justify-center outline-0 text-[28px] bg-lightGray/5 border-b-[1px] border-lightGray">
           제목
         </div>
         <div className="text-right text-deepGray">#태그 #태그 #태그</div>
