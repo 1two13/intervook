93b8732b7cc5a0a4ccb94a6ed30d2fbc45fd0a6c
Tory <88531407+1two13@users.noreply.github.com>
2023-02-14 22:12:39 +0900

    feat: 퀴즈 페이지 css 작성

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -1,5 +1,17 @@
+import QuizCard from '../components/quiz/QuizCard';
+
 function QuizPage() {
-  return <div>퀴즈</div>;
+  return (
+    <div className="w-[70%] m-auto mb-[150px] mt-[7%]">
+      {/*데이터가 없어서 우선 하드코딩, 추후 map으로 데이터 보여줄 예정*/}
+      <div className="grid grid-cols-2 gap-10 mt-[0.5%]">
+        <QuizCard category={'프론트'} type={'단답형'} title={'캐시 메모리'} />
+        <QuizCard category={'프론트'} type={'서술형'} title={'캐시 메모리'} />
+        <QuizCard category={'CS'} type={'단답형'} title={'캐시 메모리'} />
+        <QuizCard category={'CS'} type={'서술형'} title={'캐시 메모리'} />
+      </div>
+    </div>
+  );
 }
 
 export default QuizPage;
