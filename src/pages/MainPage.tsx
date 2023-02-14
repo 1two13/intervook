2bcf66aea8179cddabb1b59fd678b073ff97d33d
Tory <88531407+1two13@users.noreply.github.com>
2023-02-14 21:09:50 +0900

    refactor: 사용하지 않는 css 제거

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,7 +4,7 @@ import Card from '../components/main/Card';
 
 function MainPages() {
   return (
-    <div className="w-[70%] m-auto mb-[150px]">
+    <div className="w-[70%] m-auto">
       <SearchNBtn />
       <Buttons />
       {/*데이터가 없어서 우선 하드코딩, 추후 map으로 데이터 보여줄 예정*/}
