7fb339e48ab04cca91be69c4f61027ebe8773f5f
Tory <88531407+1two13@users.noreply.github.com>
2023-02-14 21:21:09 +0900

    refactor: 바뀐 컴포넌트명에 따라 업데이트

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,6 @@
 import SearchNBtn from '../components/main/SearchNBtn';
 import Buttons from '../components/main/Buttons';
-import Card from '../components/main/Card';
+import WritingCard from '../components/main/WritingCard';
 
 function MainPages() {
   return (
@@ -9,16 +9,16 @@ function MainPages() {
       <Buttons />
       {/*데이터가 없어서 우선 하드코딩, 추후 map으로 데이터 보여줄 예정*/}
       <div className="grid grid-cols-2 gap-10 mt-[0.5%]">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <WritingCard />
+        <WritingCard />
+        <WritingCard />
+        <WritingCard />
+        <WritingCard />
+        <WritingCard />
+        <WritingCard />
+        <WritingCard />
+        <WritingCard />
+        <WritingCard />
       </div>
     </div>
   );
