e787505c7e154849675b6c067611e1ccf7a66e7b
Tory <88531407+1two13@users.noreply.github.com>
2023-02-21 22:53:44 +0900

    style: 로그인,회원가입 공통 박스 배경 색상 변경

--- src/components/common/components/LoginJoinBox.tsx
+++ src/components/common/components/LoginJoinBox.tsx
@@ -4,7 +4,7 @@ interface ILoginJoinBox {
 
 function LoginJoinBox({ children }: ILoginJoinBox) {
   return (
-    <div className="flex flex-col max-w-[27%] px-[2.5%] py-[3%] justify-center items-center border-[0.7px] border-deepGray rounded-[15px] bg-[white]">
+    <div className="flex flex-col max-w-[27%] px-[2.5%] py-[3%] justify-center items-center border-[0.7px] border-lightGray rounded-[15px] shadow bg-lightGray/5">
       {children}
     </div>
   );
