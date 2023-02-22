f2df66d5b2a6e2f4fc3834e65fb1e05dade45ec6
Tory <88531407+1two13@users.noreply.github.com>
2023-02-22 10:53:58 +0900

    feat: 클릭한 버튼 색상 변경하기

--- src/components/common/components/Button.tsx
+++ src/components/common/components/Button.tsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
+
 interface IButton {
   name: string;
   className?: string;
 }
 
 function Button({ name, className }: IButton) {
+  const [clicked, setClicked] = useState(false);
+  const onClick = () => setClicked(!clicked);
+
   return (
     <div
-      className={`flex w-[81px] h-[36px] mr-[18px] pb-[1px] items-center justify-center cursor-pointer text-[14px] border-[2px] rounded-[80px] border-lightGray bg-lightGray/5 ${className}`}
+      className={`flex w-[81px] h-[36px] mr-[18px] pb-[1px] items-center justify-center cursor-pointer text-[14px] border-[2px] rounded-[80px] border-lightGray 
+      ${className} ${
+        clicked ? 'border-deepGreen bg-deepGreen/10 text-deepGreen font-semibold' : 'bg-lightGray/5'
+      }`}
+      onClick={() => onClick()}
     >
       {name}
     </div>
