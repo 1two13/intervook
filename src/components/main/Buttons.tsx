915573030fb7eda12780cf493e9c0ab880c65b8c
Tory <88531407+1two13@users.noreply.github.com>
2023-02-22 10:54:17 +0900

    refactor: 주석 추가

--- src/components/main/Buttons.tsx
+++ src/components/main/Buttons.tsx
@@ -3,6 +3,7 @@ import Button from '../common/components/Button';
 function Buttons() {
   return (
     <div className="flex">
+      {/* 최초 발행 시간을 알아야 하는데 서버에서 데이터를 넘겨줘야 이 부분은 가능할 것 같다. 일단 해당 버튼은 삭제될 수도 있음 */}
       <Button name={'최신 순'} />
       <Button name={'이름 순'} />
       <Button name={'좋아요 순'} />
