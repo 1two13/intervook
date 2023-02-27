d444e3cd080d84855a01fe7e9fa608b4e05099c1
Tory <88531407+1two13@users.noreply.github.com>
2023-02-27 22:20:32 +0900

    style: 카드 페이지 CSS 변경

--- src/pages/CardPage.tsx
+++ src/pages/CardPage.tsx
@@ -4,7 +4,7 @@ function CardPage() {
   };
 
   return (
-    <div className="w-[70%] m-auto px-[3%] pt-[2%] bg-lightGray/5 border-[1px] border-lightGray">
+    <div className="w-[70%] h-[100vh] m-auto px-[3%] pt-[2%] bg-lightGray/5 border-[1px] border-lightGray border-y-0">
       <div className="mb-[2%]">
         <div>작성자</div>
         <div className="w-[100%] h-[55px] justify-center outline-0 text-[28px] bg-lightGray/5 border-b-[1px] border-lightGray">
@@ -14,9 +14,10 @@ function CardPage() {
         <div className="text-right text-deepGray">#태그</div>
       </div>
 
-      <div className="h-[100vh] mb-[1%]">작성된 글...</div>
-      <div onClick={link} className="w-[100%] mb-[5%] rounded-[5%] cursor-pointer bg-bgColor">
-        <div className="h-[100px] p-[3%]">제목</div>
+      <div className="mb-[1%] p-[1%]">작성된 글...</div>
+
+      <div className="flex w-[100%] h-[60px] border-[1px] border-lightGray">
+        <div className="h-[100px] p-[3%] text-lightGray">url</div>
       </div>
     </div>
   );
