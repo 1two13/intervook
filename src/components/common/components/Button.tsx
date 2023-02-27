eed952220a5a12cd16d15c96426b8ea2290e94b5
Tory <88531407+1two13@users.noreply.github.com>
2023-02-27 22:35:10 +0900

    refactor: props로 onClick 이벤트 처리할 수 있도록 수정

--- src/components/common/components/Button.tsx
+++ src/components/common/components/Button.tsx
@@ -1,25 +1,21 @@
-import { useState } from 'react';
-
 interface IButton {
   name: string;
   className?: string;
+  onClick?: any;
+  clicked?: boolean;
 }
 
-function Button({ name, className }: IButton) {
-  const [clicked, setClicked] = useState(false);
-  const onClick = () => setClicked(!clicked);
-
+function Button({ name, className, onClick, clicked }: IButton) {
   return (
     <div
-      className={`flex w-[81px] h-[36px] mr-[18px] pb-[1px] items-center justify-center cursor-pointer text-[14px] border-[2px] rounded-[80px] border-lightGray 
+      className={`flex w-[81px] h-[36px] mr-[1%] pb-[1px] items-center justify-center cursor-pointer text-[14px] border-[2px] rounded-[80px] border-lightGray 
       ${className} ${
         clicked ? 'border-deepGreen bg-deepGreen/10 text-deepGreen font-semibold' : 'bg-lightGray/5'
       }`}
-      onClick={() => onClick()}
+      onClick={onClick}
     >
       {name}
     </div>
   );
 }
-
 export default Button;
