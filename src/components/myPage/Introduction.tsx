60eca95c518c2698cec9f05ba8116d5bfbb231fb
Tory <88531407+1two13@users.noreply.github.com>
2023-03-08 09:02:25 +0900

    feat: 자기소개가 200자를 초과하지 않도록 제한

--- src/components/myPage/Introduction.tsx
+++ src/components/myPage/Introduction.tsx
@@ -1,12 +1,38 @@
+import { useRef, useState } from 'react';
+
 function Introduction() {
+  const MAX_LENGTH = 200;
+  const [text, setText] = useState('');
+  const textarea = useRef(null);
+
+  const resizeHeight = (
+    textarea: React.RefObject<HTMLTextAreaElement>,
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    if (textarea.current) {
+      textarea.current.style.height = 'auto';
+      textarea.current.style.height = textarea.current.scrollHeight + 'px';
+      setText(e.target.value.slice(0, MAX_LENGTH));
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="w-[50%]">
-        <div className="text-[18px]">자기소개</div>
-        <input
+        <div className="flex justify-between">
+          <div className="text-[18px]">자기소개</div>
+          <div>
+            ({text.length}/{MAX_LENGTH})
+          </div>
+        </div>
+
+        <textarea
+          className="flex w-full mb-[11%] p-[2%] focus:outline-none placeholder:text-deepGray shadow-md bg-lightGray/10"
           placeholder="프론트엔드 개발자가 되고 싶은 고주디입니다!"
-          className="flex w-[100%] h-[140px] mb-[120px] p-[15px] justify-between text-[black] shadow-md bg-lightGray/10 focus:outline-none"
-        />
+          ref={textarea}
+          value={text}
+          onChange={(e) => resizeHeight(textarea, e)}
+        ></textarea>
       </div>
     </div>
   );
