23b0ae9d2d9d9ace5bfa3737da4bc4fe6a1db6fe
Tory <88531407+1two13@users.noreply.github.com>
2023-03-08 19:45:03 +0900

    feat: 모달 구현을 위한 패키지 설치, 정답을 보여주는 모달창 구현

--- src/components/common/components/BgGreenBtn.tsx
+++ src/components/common/components/BgGreenBtn.tsx
@@ -7,7 +7,8 @@ interface IBgGreenBtn {
 function BgGreenBtn({ name, onClick, className }: IBgGreenBtn) {
   return (
     <button
-      className={`w-[8vw] h-[40px] rounded-[10px] bg-deepGreen text-[white] ${className} onClick={onClick}`}
+      className={`w-[8vw] h-[40px] rounded-[10px] bg-deepGreen text-[white] ${className}`}
+      onClick={onClick}
     >
       {name}
     </button>
--- src/pages/QuizDetailsPage.tsx
+++ src/pages/QuizDetailsPage.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Modal from 'react-modal';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
@@ -7,8 +9,13 @@ import BgGreenBtn from '../components/common/components/BgGreenBtn';
 
 function QuizDetailsPage() {
   const navigate = useNavigate();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   const moveToBackPage = () => {
     navigate(-1);
+  };
+  const showAnswer = () => {
+    setModalIsOpen(!modalIsOpen);
   };
 
   return (
@@ -20,6 +27,7 @@ function QuizDetailsPage() {
         onClick={moveToBackPage}
       />
       <h1 className="text-[45px]">문제 제목</h1>
+
       <div className="my-[2%] p-[2%] h-[50vh] shadow-md bg-lightGray/10">
         <h1 className="text-2xl font-semibold">문제 설명</h1>
         <div className="h-[67%] pt-[3%]">주절주절</div>
@@ -28,7 +36,18 @@ function QuizDetailsPage() {
           className="w-[100%] h-[15%] mt-[2%] px-[1%] border-[1px] border-lightGray outline-0"
         />
       </div>
-      <BgGreenBtn name="정답확인" className="float-right" />
+      <BgGreenBtn name="정답확인" className="float-right" onClick={showAnswer} />
+      {modalIsOpen ? (
+        <Modal
+          isOpen={true}
+          ariaHideApp={false}
+          className="w-[50%] h-[58vh] mt-[10%] m-auto px-[7%] py-[3%] bg-bgColor text-lg border-[1px] border-lightGray rounded-[10px]"
+          onRequestClose={() => setModalIsOpen(false)}
+        >
+          <h1 className="flex justify-center mb-[5%] text-5xl text-[red]">정답</h1>
+          정답은 ...입니다
+        </Modal>
+      ) : null}
     </div>
   );
 }
