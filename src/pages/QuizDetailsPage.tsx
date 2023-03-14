74b8573e07d4d4930c53802e1f20184bbd7a96fc
Tory <88531407+1two13@users.noreply.github.com>
2023-03-14 19:36:28 +0900

    refactor: 모달창 수정

--- src/pages/QuizDetailsPage.tsx
+++ src/pages/QuizDetailsPage.tsx
@@ -36,16 +36,18 @@ function QuizDetailsPage() {
           className="w-[100%] h-[15%] mt-[2%] px-[1%] border-[1px] border-lightGray outline-0"
         />
       </div>
-      <BgGreenBtn name="정답확인" className="float-right" onClick={showAnswer} />
+      <BgGreenBtn name="정답 확인" className="float-right" onClick={showAnswer} />
       {modalIsOpen ? (
         <Modal
           isOpen={true}
           ariaHideApp={false}
-          className="w-[50%] h-[58vh] mt-[10%] m-auto px-[7%] py-[3%] bg-bgColor text-lg border-[1px] border-lightGray rounded-[10px]"
+          className="w-[50%] h-[58vh] mt-[10%] m-auto bg-bgColor text-lg rounded-[10px] drop-shadow-lg"
           onRequestClose={() => setModalIsOpen(false)}
         >
-          <h1 className="flex justify-center mb-[5%] text-5xl text-[red]">정답</h1>
-          정답은 ...입니다
+          <h1 className="flex justify-center h-[20%] items-center text-5xl bg-lightGreen rounded-t-[10px]">
+            정답
+          </h1>
+          <div className="p-[5%]">정답은 ...입니다</div>
         </Modal>
       ) : null}
     </div>
