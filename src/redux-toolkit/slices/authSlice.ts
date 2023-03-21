cc6815fe5e25021318794c77c24b347a5a3f0b63
Tory <88531407+1two13@users.noreply.github.com>
2023-03-21 18:07:14 +0900

    refactor: authSlice logout 액션 수정

--- src/redux-toolkit/slices/authSlice.ts
+++ src/redux-toolkit/slices/authSlice.ts
@@ -1,17 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+interface IInitialState {
+  email: string;
+  username: string;
+  isLogin: boolean;
+}
+
+const initialState: IInitialState = { email: '', username: '', isLogin: false };
+
 export const authSlice = createSlice({
   name: 'auth',
-  initialState: { email: 0, username: '', isLogin: false },
+  initialState,
   reducers: {
     login: (state, action) => {
       state.email = action.payload;
       state.isLogin = true;
     },
-    logout: (state, action) => {
-      state.email = 0;
-      state.username = '';
-      state.isLogin = false;
-    },
+    logout: () => initialState,
   },
 });
